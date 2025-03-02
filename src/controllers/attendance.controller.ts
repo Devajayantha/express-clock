00ada2b1834d115615218a31cc5137fbc29743f8
Devajayantha <devajayantha888@gmail.com>
2025-03-03 00:17:13 +0800

    - add attendance migration - attendance clock in api

---
+++ src/controllers/attendance.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { returnResponse } from "../utils/response.utils";
+import { StatusCode } from "../enums/status-code.enum";
+import { AuthRequest } from "../interfaces/auth.interface";
+import prisma from "../configs/prisma.config";
+
+export async function clockIn(req: Request, res: Response) {
+  try {
+    const authReq = req as AuthRequest;
+    const user = authReq.user;
+
+    const existAttendance = await prisma.$queryRawUnsafe<any[]>(`
+      SELECT * FROM attendances
+      WHERE userId = '${user.id}'
+      AND DATE(created_at) = CURDATE()
+      LIMIT 1
+    `);
+
+    if (existAttendance.length > 0) {
+      returnResponse(res, StatusCode.BAD_REQUEST, false, [], "You have already clocked in today");
+      return;
+    }
+
+    const attendance = await prisma.attendance.create({
+      data: {
+        userId: user.id,
+        clockIn: new Date()
+      }
+    });
+
+    returnResponse(res, StatusCode.OK, true, { user }, "User clocked in successfully");
+  } catch (error) {
+    returnResponse(res, StatusCode.SERVER_ERROR, false, [], "Internal server error");
+  }
+}
--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import * as AttendanceController from '../controllers/attendance.controller'; 
 import * as UserController from '../controllers/user.controller';
 import { validate } from '../utils/validation.utils';
 import { loginRequest } from '../requests/login.request';
+import { authenticate } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
 router.post("/auth/login", validate(loginRequest), UserController.login);
+
+router.post("/attendances/clock-in", authenticate, AttendanceController.clockIn)
 
 export default router;
